55470eb95eb8900d47458d47e38ed27ee5ad8169
Julio <148634894+Jbarbosaa@users.noreply.github.com>
2025-08-01 14:32:46 -0300

    Correção das variáveis do script

--- script-input-data-to-db.sh
+++ script-input-data-to-db.sh
@@ -2,12 +2,12 @@
 
 counter=0
 
-while [ $counter -lt 50 ]; do
+while [ $counter -lt 20 ]; do
         let counter=counter+1
 
-        name= $(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $1}')
-        lastname= $(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $2}')
-        age= $(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $3}')
+        name=$(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $1}')
+        lastname=$(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $2}')
+        age=$(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $3}')
         echo "Name: $name, Lastname: $lastname, Age: $age"
         echo "INSERT INTO people (name, lastname) VALUES ('$name', '$lastname');" >> insert_people.sql
         sed -i '1d' people.txt
