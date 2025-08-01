8b9b06c6d067ba1e3b06d6e74845aa3e597bd416
Julio <148634894+Jbarbosaa@users.noreply.github.com>
2025-08-01 15:29:56 -0300

    Correção na definição da variável

--- script-input-data-to-db.sh
+++ script-input-data-to-db.sh
@@ -6,10 +6,10 @@ while [ $counter -lt 20 ]; do
         let counter=counter+1
 
         line=$(head -n 1 people.txt)
-        name=$(echo "line" | awk -F ',' '{print $1}')
-        lastname=$(echo "line" | awk -F ',' '{print $2}')
-        age=$(echo "line" | awk -F ',' '{print $3}')
-        
+        name=$(echo "$line" | awk -F ',' '{print $1}')
+        lastname=$(echo "$line" | awk -F ',' '{print $2}')
+        age=$(echo "$line" | awk -F ',' '{print $3}')
+
         echo "Name: $name, Lastname: $lastname, Age: $age"
         echo "INSERT INTO people (name, lastname) VALUES ('$name', '$lastname');" >> insert_people.sql
         sed -i '1d' people.txt
