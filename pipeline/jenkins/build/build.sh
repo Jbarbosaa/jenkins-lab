e37c7f137d66b2075505d323a2ba071f0696f2bd
Julio Barbosa <julio.almeida31@hotmail.com>
2025-08-15 18:21:06 -0300

    Testing

--- pipeline/jenkins/build/build.sh
+++ pipeline/jenkins/build/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # Copy the new jar to the build location
-
+echo "antes do cd"
 pwd
-cp -f /target/*.jar ./app.jar
+
+cd /home/jenkins/jenkins-data/pipeline/java-app/target/
+echo "dps do cd"
+pwd
+
+cp -f *.jar ./app.jar
 
 echo "*********************************"
 echo "**** Building Docker Images *****"
