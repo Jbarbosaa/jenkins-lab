8a07a980656dd6e5eaedd9b7c36718afc7196c9e
Julio Barbosa <julio.almeida31@hotmail.com>
2025-08-16 00:47:34 -0300

    Adding final deploy stage, to connect on EC2-AWS and do maven-app deploy

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
+
 echo maven-project > /tmp/.env
-echo $BUILD_TAG >> /tmp/.env
-echo $pass >> /tmp/.env
+echo $BUILD_TAG > /tmp/.env
+echo $pass > /tmp/.env
 
 sed -i '1s/^/IMAGE=/' /tmp/.env
 sed -i '2s/^/TAG=/' /tmp/.env
 sed -i '3s/^/pass=/' /tmp/.env
 
+KEYDIRECTORY=/var/jenkins_home/.ssh
 
-scp -i ~/.ssh/prod /tmp/.env prod-user@54.207.139.47:/tmp/.env
-scp -i ~/.ssh/prod /home/jenkins/jenkins-data/pipeline/jenkins/deploy/publish.sh prod-user@54.207.139.47:/tmp/publish.sh
+scp -i $KEYDIRECTORY /tmp/.env prod-user@54.207.139.47:/tmp/.env
+scp -i $KEYDIRECTORY /home/jenkins/jenkins-data/pipeline/jenkins/deploy/docker-compose.yml prod-user@54.207.139.47:/tmp/docker-compose.yml
 
-#ssh -i ~/.ssh/prod prod-user@54.207.139.47 './tmp/publish.sh'
+#ssh -i $KEYDIRECTORY prod-user@54.207.139.47 './tmp/publish.sh'
 
-ssh -i ~/.ssh/prod prod-user@54.207.139.47 'bash /tmp/publish.sh'
+ssh -i $KEYDIRECTORY prod-user@54.207.139.47 'cd /tmp && docker-compose up -d'
