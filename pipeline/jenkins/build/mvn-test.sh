0ffec483b8fa6fef7ef090ae9edf0fa84f78a6b4
Julio Barbosa <julio.almeida31@hotmail.com>
2025-08-16 00:04:53 -0300

    testing

--- pipeline/jenkins/build/mvn-test.sh
+++ pipeline/jenkins/build/mvn-test.sh
@@ -2,11 +2,13 @@
 
 # Testing the .jar
 
-cp -f java-app/target/*jar jenkins/build
+cp -f /var/java-app/target/*.jar ./pipeline/jenkins/build/app.jar
 
 echo "*********************************"
 echo "**** Testing the code *****"
 echo "*********************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven
-docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.9.9-eclipse-temurin-21 "$@"
+docker run --rm -v /home/jenkins/jenkins-data/pipeline/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.9.9-eclipse-temurin-21 mvn "$@"
+
+#WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven
+#docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.9.9-eclipse-temurin-21 "$@"
