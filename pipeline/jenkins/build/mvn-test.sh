1cf68fd3acea9416d8853312ed682e2a90eef413
Julio Barbosa <julio.almeida31@hotmail.com>
2025-08-15 22:52:50 -0300

    fixes variables patch

--- pipeline/jenkins/build/mvn-test.sh
+++ pipeline/jenkins/build/mvn-test.sh
@@ -8,4 +8,5 @@ echo "*********************************"
 echo "**** Testing the code *****"
 echo "*********************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.9.9-eclipse-temurin-21 "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2 -w /app maven:3.9.9-eclipse-temurin-21 "$@"
